c804e401a10a1bd55d5d6cdfe25ffa42735acae3
anatolB <a.birtanov@takeoff-staff.ru>
2024-09-09 08:53:33 +0300

    mod

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 export  default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="px-6 py-4 h-screen w-full bg-gray-100 flex flex-col items-center">
+    <div className="px-6 py-4 min-h-screen w-full bg-gray-100 flex flex-col items-center">
       <h1 className="text-2xl font-semibold mb-4">Dynamic Form Builder</h1>
       {children}
     </div>
